50db5cc53b7cfbef13e8db55e2d054846ff29d45
dinesh-58 <86787475+dinesh-58@users.noreply.github.com>
2023-11-16 23:15:28 +0545

    init

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+    return (
+        <>
+            <button class="btn btn-primary">Button</button>
+        </>
+    )
+}
+
+export default App
