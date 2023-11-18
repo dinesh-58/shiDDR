3ade30d96445c00aaaf48898fe19c489ff71b581
dinesh-58 <86787475+dinesh-58@users.noreply.github.com>
2023-11-18 11:16:17 +0545

    compare player click with bot sequence

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,11 @@ import Board from './components/Board.jsx'
 import './App.css'
 
 export default function App() {
+    /* NOTE: 
+        * Pad refers to the squares that change color
+        * Board refers to the container that stores 9 pads
+    */
+
     function initDefaultBoard() {
         return [ false, false, false, false, false, false, false, false, false ]; 
         // 9 booleans that represent each pad's clicked state
@@ -13,14 +18,26 @@ export default function App() {
 
     const [botBoard, setBotBoard] = useState(initDefaultBoard);
     const [playerBoard, setPlayerBoard] = useState(initDefaultBoard);
+    const [botSequence, setBotSequence] = useState([])
 
-    function togglePad(event, id) {
-        // event isn't used but is put here because onclick always passes it
+    function togglePad(id) {
         // setterFunction = isPlayerBoard ? setPlayerBoard : setBotBoard;
         // only player board for now? idk
         setPlayerBoard(playerBoard.map((padState, i) => 
             i === id ? !padState : padState
         ))
+    }
+
+    function handlePlayerClick(event, id) {
+        // event isn't used but is put here because onclick always passes it
+            // could use even.target.id later if I decide to store board as array of objects
+        togglePad(id);
+        // check if clicked pad matches bot generated pad
+        
+        if(id == botSequence[0]) console.log("correct")
+        else console.log("wrong")
+        // check state of board as well? i.e. both player and bot are enabled/disabled at clicked spot
+        // playerBoard[id] == botBoard
     }
 
     function generateBotSequence() {
@@ -37,6 +54,8 @@ export default function App() {
         setBotBoard(botBoard.map((padState, i) => 
             sequence.includes(i) ? true : false
         ));
+        // gonna have to use additional logic later for setting bot sequence based on prev value
+        setBotSequence(sequence);
     }
 
     return (
@@ -44,7 +63,7 @@ export default function App() {
         grid-cols-2 
         gap-8 justify-evenly">
             <Board boardType="bot" board={botBoard} />
-            <Board boardType="player" board={playerBoard} padClick={togglePad}/>
+            <Board boardType="player" board={playerBoard} padClick={handlePlayerClick}/>
             <button className="btn btn-primary"
             onClick={generateBotSequence}
         >
